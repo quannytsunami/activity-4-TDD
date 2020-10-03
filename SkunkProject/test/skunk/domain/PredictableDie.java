61a6ac7f6798d53a1e6b21c3a51f98ce1be9a16d
quannytsunami <nguy1491@gmail.com>
2020-10-02 20:34:21 -0500

    3rd commit of PDie

--- SkunkProject/test/skunk/domain/PredictableDie.java
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -14,6 +14,10 @@ public class PredictableDie {
 	public void roll() {
 		this.lastRoll = this.theRolls[this.nextInt];
 		this.nextInt++;
+		if (this.nextInt == this.theRolls.length)
+		{
+			this.nextInt = 0;
+		}
 		
 	}
 
--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -14,6 +14,17 @@ class TestPredictableDie {
 		assertEquals(1, die.getLastRoll());
 		die.roll();
 		assertEquals(2, die.getLastRoll());
+		die.roll();
+		assertEquals(3, die.getLastRoll());
 	}
-
+	
+	@Test
+	void test_PD_1_more_than_once()
+	{
+		PredictableDie die = new PredictableDie(new int[] {1});
+		die.roll();
+		assertEquals(1, die.getLastRoll());
+		die.roll();	
+		assertEquals(1, die.getLastRoll());
+	}
 }
